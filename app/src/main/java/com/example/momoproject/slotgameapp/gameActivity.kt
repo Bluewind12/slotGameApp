da5ecb85be58b3212065e169ac3840fa614ce544
Bluewind12 <zero18030@gmail.com>
2018-07-05 20:22:24 +0900

    game_system_end

--- app/src/main/java/com/example/momoproject/slotgameapp/gameActivity.kt
+++ app/src/main/java/com/example/momoproject/slotgameapp/gameActivity.kt
@@ -1,5 +1,7 @@
 package com.example.momoproject.slotgameapp
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
@@ -8,6 +10,7 @@ import android.widget.TextView
 import java.util.*
 
 class gameActivity : AppCompatActivity() {
+    //入れ子
     private lateinit var leftImage: ImageView
     private lateinit var centerImage: ImageView
     private lateinit var rightImage: ImageView
@@ -16,16 +19,26 @@ class gameActivity : AppCompatActivity() {
     private lateinit var stopButton3: Button
     private lateinit var resetButton: Button
     private lateinit var resultText: TextView
+    private lateinit var coinText: TextView
+    //イメージデータ配列
+    val imageData = listOf(R.drawable.irasuto1, R.drawable.irasuto2, R.drawable.irasuto3, R.drawable.irasuto4, R.drawable.irasuto5)
     //ランダム用
     private val random = Random()
-
     //揃ったかどうかの判定用
     var leftNum = 99
     var centerNum = 99
     var rightNum = 99
 
-    //イメージデータ配列
-    val imageData = listOf(R.drawable.irasuto1, R.drawable.irasuto2, R.drawable.irasuto3, R.drawable.irasuto4, R.drawable.irasuto5)
+    //データ管理用
+    private lateinit var data: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
+    //所持コインなどデータ
+    private var coin = 0
+    private var playCount = 0
+    private var winCount = 0
+
+    //デバック用
+    private val imageSelectNum = 2;// 1～5まで
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,22 +47,21 @@ class gameActivity : AppCompatActivity() {
         //TODO　ゲーム画面の作成
         //TODO　掛け金、コインの設定
         init()
-        resetButton.isEnabled = false
-
+        startSet()
         stopButton1.setOnClickListener {
-            leftNum = random.nextInt(5)
+            leftNum = random.nextInt(imageSelectNum)
             leftImage.setImageResource(imageData[leftNum])
             stopButton1.isEnabled = false
             checkSlot()
         }
         stopButton2.setOnClickListener {
-            centerNum = random.nextInt(5)
+            centerNum = random.nextInt(imageSelectNum)
             centerImage.setImageResource(imageData[centerNum])
             stopButton2.isEnabled = false
             checkSlot()
         }
         stopButton3.setOnClickListener {
-            rightNum = random.nextInt(5)
+            rightNum = random.nextInt(imageSelectNum)
             rightImage.setImageResource(imageData[rightNum])
             stopButton3.isEnabled = false
             checkSlot()
@@ -83,14 +95,45 @@ class gameActivity : AppCompatActivity() {
         stopButton3 = findViewById(R.id.stopButton3)
         resetButton = findViewById(R.id.resetButton)
         resultText = findViewById(R.id.resultText)
+        coinText = findViewById(R.id.coinText)
+        data = getSharedPreferences("MainData", Context.MODE_PRIVATE)
+        editor = data.edit()
     }
 
-    fun checkSlot() {
+    private fun startSet() {
+        coin = data.getInt("coin", 500);
+        playCount = data.getInt("play", 0);
+        winCount = data.getInt("win", 0);
+
+        resetButton.isEnabled = false
+        coinText.text = coin.toString()
+
+    }
+
+    private fun checkSlot() {
         if (leftNum == centerNum && leftNum == rightNum) {
+            //数値処理
+            coin += 50
+            playCount++
+            winCount++
+            editor.putInt("coin", coin)
+            editor.putInt("play", playCount)
+            editor.putInt("win", winCount)
+            editor.commit()
+            //表示
             resultText.text = "おめでとう！！"
+            coinText.text = coin.toString()
             resetButton.isEnabled = true
         } else if (!stopButton1.isEnabled && !stopButton2.isEnabled && !stopButton3.isEnabled) {
+            //数値処理
+            coin -= 10
+            playCount++
+            editor.putInt("coin", coin)
+            editor.putInt("play", playCount)
+            editor.commit()
+            //表示
             resultText.text = "ざんねん！！"
+            coinText.text = coin.toString()
             resetButton.isEnabled = true
         }
     }
--- app/src/main/java/com/example/momoproject/slotgameapp/recordActivity.kt
+++ app/src/main/java/com/example/momoproject/slotgameapp/recordActivity.kt
@@ -1,13 +1,65 @@
 package com.example.momoproject.slotgameapp
 
+import android.content.Context
+import android.content.DialogInterface
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
 
-class recordActivity :AppCompatActivity(){
+class recordActivity : AppCompatActivity() {
+    private lateinit var playCountText: TextView
+    private lateinit var winCountText: TextView
+    private lateinit var winPerText: TextView
+    private lateinit var haveCoinText: TextView
+
+    private lateinit var resetButton: Button
+
+    //データ管理用
+    private lateinit var data: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.record)
 
         //TODO 履歴・勝率等の表示を行う
+        init()
+        playCountText.text = getString(R.string.count, data.getInt("play", 0))
+        winCountText.text = getString(R.string.count, data.getInt("win", 0))
+        if (data.getInt("play", 1) != 0) {
+            winPerText.text = getString(R.string.per, data.getInt("win", 0).toDouble() / data.getInt("play", 1).toDouble() * 100.0)
+        } else {
+            winPerText.text = getString(R.string.per, data.getInt("win", 0).toDouble() / 1.0 * 100.0)
+        }
+        haveCoinText.text = getString(R.string.have_coin, data.getInt("coin", 500))
+
+        resetButton.setOnClickListener {
+            AlertDialog.Builder(this).apply {
+                setTitle("削除")
+                setMessage("今までの記録を削除してもいいですか？")
+                setPositiveButton("はい", DialogInterface.OnClickListener { dialog, which ->
+                    editor.putInt("play", 0)
+                    editor.putInt("win", 0)
+                    editor.putInt("coin", 500)
+                    editor.commit()
+                })
+                setNegativeButton("いいえ", null)
+                show()
+            }
+        }
+    }
+
+    private fun init() {
+        playCountText = findViewById(R.id.playCountTextView)
+        winCountText = findViewById(R.id.winCountTextView)
+        winPerText = findViewById(R.id.winPerTextView)
+        haveCoinText = findViewById(R.id.coinTextView)
+
+        resetButton = findViewById(R.id.recordResetButton)
+        data = getSharedPreferences("MainData", Context.MODE_PRIVATE)
+        editor = data.edit()
     }
 }
